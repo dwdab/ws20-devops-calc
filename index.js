e0899e0b389567cced78b57f500ef1d6fee05e33
Stefan Groneberg <stefan.groneberg@gmx.de>
2021-01-24 15:37:13 +0100

    #

--- index.js
+++ index.js
@@ -4,62 +4,19 @@
 
 var arguments = process.argv.slice(2);
 
-var operand1 = toNumber(arguments[0], 10);
-var operand2 = toNumber(arguments[1], 10);
+var operand1 = arguments[0];
+var operand2 = arguments[1];
 var operator = arguments[2];
-var result = 0;
 
 console.log('Calculating:', operand1, operand2, operator);
 
-calculateSetOperation();
+var result;
 
-// selects the correct case and performs the correct calculation depending on the operands
-function calculateSetOperation() {
-
-  switch (operator) {
-    case '+':
-      result = addition(operand1, operand2);
-      break;
-    case '-':
-      result = subtraction(operand1, operand2);
-      break;
-      case '*':
-        result = multiplication(operand1, operand2);
-        break;
-    /*
-        case '/':
-        result = division(operand1, operand2);
-        break;
-    */
-    default:
-      console.error('Not implemented:', operator);
-  }
-  console.log(result);
+switch (operator) {
+  case '+':
+    result = operand1 + operand2;
+  default:
+    console.error('Not implemented:', operator);
 }
 
-// returns the parsing from a string to a number
-function toNumber(x) {
-  return parseInt(x, 10);
-}
-
-// performs an addition of two parameters
-function addition(x, y) {
-  return x + y;
-}
-
-// performs a subtraction of the second parameter from the first one.
-function subtraction(x, y) {
-  return x - y;
-}
-
-// performs a multiplication of two parameters
-function multiplication(x,y) {
-  return x * y;
-}
-
-/*
-// performs a division of first parameters by the second one
-function division(x,y) {
-  return x / y;
-}
-*/
+console.log(result);
