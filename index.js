3e4c33bee0d3303e8a4198d7d4335606c7f60e5a
Erdem Temizdemir <55912553+gh03ty@users.noreply.github.com>
2021-01-25 14:23:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,44 @@
  * Reverse Polish Notation Calculator
  */
 
-var arguments = process.argv.slice(2);
+let arguments = process.argv.slice(2);
 
-var operand1 = arguments[0];
-var operand2 = arguments[1];
-var operator = arguments[2];
+/* FIX #4 - Calculation is not possible with Strings */
+let operand1 = parseInt(arguments[0], 10);
+let operand2 = parseInt(arguments[1], 10);
+/* _________________________________________________ */
+let operator = arguments[2];
 
 console.log('Calculating:', operand1, operand2, operator);
 
-var result;
+let result;
 
 switch (operator) {
   case '+':
     result = operand1 + operand2;
+    /* FIX #3 & #7 - Cases have to be breaked to end operations */
+    break;
+    /* ________________________________________________________ */
+  /* FIX #6 - Missing operators added                      */
+  case '-':
+    result = operand1 - operand2;
+    break;
+  case '*':
+    result = operand1 * operand2;
+    break;
+  case '/':
+    result = operand1 / operand2;
+    break;
+  case 'p':
+    result = Math.pow(operand1, operand2);
+    break;
+  /*                                                        */
   default:
-    console.error('Not implemented:', operator);
+    console.error('Not implemented or Is not a mathamatical operator :', operator);
+    /* FIX #5 - Additional help for userfriendliness added           */
+    console.log('Supported operators are: + , - , / , * , p (power).');
+    /* _____________________________________________________________ */
+    break;
 }
 
 console.log(result);
