c6840366611ac26e7bf8f0730bcd7ac0b894cbf7
Erdem Temizdemir <55912553+gh03ty@users.noreply.github.com>
2021-01-26 18:22:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,42 +4,64 @@
 
 let arguments = process.argv.slice(2);
 
-/* FIX #4 - Calculation is not possible with Strings */
 let operand1 = parseInt(arguments[0], 10);
 let operand2 = parseInt(arguments[1], 10);
-/* _________________________________________________ */
 let operator = arguments[2];
+let result = 0;
 
 console.log('Calculating:', operand1, operand2, operator);
 
-let result;
+calculateSetOperation();
 
-switch (operator) {
-  case '+':
-    result = operand1 + operand2;
-    /* FIX #3 & #7 - Cases have to be breaked to end operations */
-    break;
-    /* ________________________________________________________ */
-  /* FIX #6 - Missing operators added                      */
-  case '-':
-    result = operand1 - operand2;
-    break;
-  case '*':
-    result = operand1 * operand2;
-    break;
-  case '/':
-    result = operand1 / operand2;
-    break;
-  case 'p':
-    result = Math.pow(operand1, operand2);
-    break;
-  /*                                                        */
-  default:
-    console.error('Not implemented or Is not a mathamatical operator :', operator);
-    /* FIX #5 - Additional help for userfriendliness added           */
-    console.log('Supported operators are: + , - , / , * , p (power).');
-    /* _____________________________________________________________ */
-    break;
+// selects the correct case and performs the correct calculation depending on the operands
+function calculateSetOperation() {
+  switch (operator) {
+    case '+':
+      result = addition(operand1, operand2);
+      break;
+    case '-':
+      result = subtraction(operand1, operand2);
+      break;
+    case '*':
+      result = operand1 * operand2;
+      break;
+    case '/':
+      result = operand1 / operand2;
+      break;
+    case 'p':
+      result = Math.pow(operand1, operand2);
+      break;
+    default:
+      console.error('Not implemented or is not a mathematical operator:', operator);
+      console.log('Supported operators are: + , - , / , * , p (power).');
+  }
+
+  console.log(result);
 }
 
-console.log(result);
+// returns the parsing from a string to a number
+function toNumber(x) {
+  return parseInt(x, 10);
+}
+
+// performs an addition of two parameters
+function addition(x, y) {
+  return x + y;
+}
+
+// performs a subtraction of the second parameter from the first one.
+function subtraction(x, y) {
+  return x - y;
+}
+
+/*
+// performs a multiplication of two parameters
+function multiplication(x,y) {
+  return x * y;
+}
+
+// performs a division of first parameters by the second one
+function division(x,y) {
+  return x / y;
+}
+*/
