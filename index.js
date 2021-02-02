ef51a33ce1c9cb435608372b5ce27197024c1672
Erdem Temizdemir <55912553+gh03ty@users.noreply.github.com>
2021-02-02 23:13:03 +0100

    Generalized Multiplication

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const addition = require('./operations/addition');
-// TODO: subtraction - Stefan
-const multiplication = require('./operations/multiplication'); // TODO: Erdem
-const division = require('./operations/division'); // TODO: Sascha
-const modulo = require('./operations/modulo'); // TODO: Steen
-const power = require('./operations/power'); // TODO: Kevin
+const multiplication = require('./operations/multiplication');
+const division = require('./operations/division');
+const modulo = require('./operations/modulo');
+const power = require('./operations/power');
 
-const operations = [addition];
+const operations = [addition, multiplication];
 
 /**
  * Reverse Polish Notation Calculator
@@ -22,39 +21,20 @@ console.log('Calculating:', operand1, operand2, operator);
 
 calculateSetOperation();
 
-// selects the correct case and performs the correct calculation depending on the operands
-function calculateSetOperation() {
-  switch (operator) {
-    case '-':
-      result = subtraction(operand1, operand2);
-      break;
-    case '*':
-      result = multiplication(operand1, operand2);
-      break;
-    case '/':
-      result = division(operand1, operand2);
-      break;
-    case 'p':
-      result = power(operand1, operand2);
-      break;
-    case '%':
-      result = modulo(operand1, operand2);
-      break;
-  }
+  function calculateSetOperation() {
+    const operation = operations.find((e) => e.operator === operator);
 
-  const operation = operations.find((e) => e.operator === operator);
+    if (operation) {
+      result = operation.fn(operand1, operand2);
+    }
 
-  if (operation) {
-    result = operation.fn(operand1, operand2);
+    if (result === null) {
+      console.error('Not implemented or is not a mathematical operator:', operator);
+      console.log('Supported operators are: + , - , / , * , p (power) and % (modulo).');
+    } else {
+      console.log('Result=', result);
+    }
   }
-
-  if (result === null) {
-    console.error('Not implemented or is not a mathematical operator:', operator);
-    console.log('Supported operators are: + , - , / , * , p (power) and % (modulo).');
-  } else {
-    console.log('Result=', result);
-  }
-}
 
 // performs a subtraction of the second parameter from the first one.
 function subtraction(x, y) {
